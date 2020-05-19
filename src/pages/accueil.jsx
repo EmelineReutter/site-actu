490debc88b254a80a9b542b93365df16adc6d1a0
Antoine Cheron <cheron.antoine@gmail.com>
2020-05-19 12:38:57 +0200

    initialise le projet react avec 2 pages

---
+++ src/pages/accueil.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Accueil () {
+  return (<section>
+    <h1>Acceuil</h1>
+    <p>Emeline, c'est ici que tu dois tester les composants que tu créé.</p>
+    <a href="/ant-design">Aller à la page ant design</a>
+
+    <div id="emeline-work">
+      Remplace ce contenu par tes composants
+    </div>
+  </section>)
+}
+
+export default Accueil
---
+++ src/pages/antdesign.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function AntDesign () {
+  return (<section>
+    <h1>Ant Design</h1>
+    <p>Emeline, c'est ici que tu peux copier/coller tout ce qui vient d'<a href="https://ant.design/components/button/">ant design.</a></p>
+    <a href="/">Aller à l'accueil</a>
+
+    <div>
+      Remplace ce contenu par les composants d'ant design
+    </div>
+  </section>)
+}
+
+export default AntDesign
